4a5b4ebd80cfb46fa3d9d09eaee85f04db27f32b
chakravarthi <60620204+chakravarthi-v@users.noreply.github.com>
2020-07-22 14:22:06 +0530

    Update pictures.java

--- app/src/main/java/com/example/instagram/pictures.java
+++ app/src/main/java/com/example/instagram/pictures.java
@@ -37,25 +37,24 @@ public class pictures extends AppCompatActivity {
         check.findInBackground(new FindCallback<ParseObject>() {
             @Override
             public void done(List<ParseObject> objects, ParseException e) {
-                Toast.makeText(pictures.this,"load2",Toast.LENGTH_SHORT).show();
                 if(e==null){
-                    Toast.makeText(pictures.this,"load2.5",Toast.LENGTH_SHORT).show();
+                  
                     for(ParseObject imgs:objects){
                         ParseFile folder=(ParseFile) imgs.get("image");
-                        Toast.makeText(pictures.this,"load2.6",Toast.LENGTH_SHORT).show();
+                       
                         assert folder != null;
                         folder.getDataInBackground(new GetDataCallback() {
                             @Override
                             public void done(byte[] data, ParseException e) {
-                                Toast.makeText(pictures.this,"load2.7",Toast.LENGTH_SHORT).show();
+                                
                                 if(e==null&&data!=null){
-                                    Toast.makeText(pictures.this,"load3",Toast.LENGTH_SHORT).show();
+                                    
                                     Bitmap bit= BitmapFactory.decodeByteArray(data,0,data.length);
                                     ImageView pico=new ImageView(getApplicationContext());
                                     pico.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,ViewGroup.LayoutParams.WRAP_CONTENT));
                                     pico.setImageBitmap(bit);
                                     grid.addView(pico);
-                                    Toast.makeText(pictures.this,"load4",Toast.LENGTH_SHORT).show();
+                               
                                 }
                                 else{
                                     assert e != null;
